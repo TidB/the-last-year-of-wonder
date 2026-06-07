9dc004ae003d3c6b70814e8761e4f197a11c7840
TidB <TidB@users.noreply.github.com>
2026-06-07 19:23:56 +0200

    Cleanup, added band

--- assets/firework/firework.gd
+++ assets/firework/firework.gd
@@ -6,22 +6,17 @@ const COLORS = [
 	Color("ffb000"),
 	Color("fe6100"),
 	Color("dc267f"),
-	Color("785ef0"),
+	Color("785ef0"),  # Might skip this one for Protanopia?
 	Color("648fff"),
 ]
 var current_color = 0
 
 func _ready():
-	self.explosions = [$explosion2, $explosion3, $explosion4]
-	
-	#for explosion in explosions:
-#		explosion.finished.connect(_on_explosion_finished)
-		#explosion.restart()
+	self.explosions = self.get_children()
 	
 	self.fire()
 
 func fire():
-	print(len(get_stack()))
 	self.explosions.shuffle()
 
 	for explosion in explosions:
@@ -30,4 +25,4 @@ func fire():
 		await get_tree().create_timer(0.2).timeout
 		
 	self.current_color = (self.current_color + 1) % 5
-	get_tree().create_timer(2.6).timeout.connect(self.fire)
+	get_tree().create_timer(2.5).timeout.connect(self.fire)
